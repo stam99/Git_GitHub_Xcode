6e00ccbf8ab3beda4b32cdec5e7791277cb51334
Serena Tam <sgt2118@columbia.edu>
2019-07-02 10:44:49 -0700

    Added GitTest.swift file

---
+++ GettingStartedFollowAlong/GitTest.swift
@@ -0,0 +1,9 @@
+//
+//  GitTest.swift
+//  GettingStartedFollowAlong
+//
+//  Created by Michael Panitz on 6/17/19.
+//  Copyright © 2019 Michael Panitz. All rights reserved.
+//
+
+import Foundation
